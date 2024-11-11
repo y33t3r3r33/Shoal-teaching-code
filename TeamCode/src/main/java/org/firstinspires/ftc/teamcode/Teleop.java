622a2a9fef027d1ee0546e1c69cd51d0e43586f0
y33t3r3r33 <dumasdd9@gmail.com>
2024-11-11 12:51:29 -0600

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+
+public class Teleop extends OpMode {
+
+    public MotorEx left;
+    public MotorEx right;
+
+    public CRServo Intake;
+
+    @Override
+    public void init() {
+    this.right = new MotorEx(hardwareMap, "Right");
+    this.left = new MotorEx(hardwareMap, "Left");
+
+    this.Intake = hardwareMap.get(CRServo.class,"Intake");
+    }
+
+    @Override
+    public void loop() {
+    double power1 = gamepad1.left_stick_y;
+    double power2 = gamepad1.right_stick_y;
+
+    double ipower = gamepad2.left_trigger;
+
+    left.set(power1);
+    right.set(power2);
+
+    Intake.setPower(ipower);
+    }
+}
